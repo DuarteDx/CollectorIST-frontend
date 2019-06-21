ccbc027a8f4c43d0674e608347555797e55da7e1
Duarte Dias <DuartexDias@gmail.com>
2019-06-21 17:12:52 +0100

    moved insert collection comp into collections list

--- src/views/Collections.vue
+++ src/views/Collections.vue
@@ -1,11 +1,8 @@
 <template>
     <div>
-        <!--<v-layout justify-center>
-            <v-flex sm5 md3 style="font-size: 20px; margin: 20px 0 10px 0;">
-                List of collections in database
-            </v-flex>
-        </v-layout>-->
         <SearchBar/>
+        <v-btn color="info" @click="displayInsertionForm()">+ Inserir coleção</v-btn>
+        <InsertSingleCollection v-if="displayInsertionFormButton"/>
         <CollectionsList/>
     </div>
 </template>
@@ -13,12 +10,24 @@
 <script>
 import CollectionsList from '@/components/collections/search/CollectionsList'
 import SearchBar from '@/components/collections/search/SearchBar'
+import InsertSingleCollection from '@/components/collections/insertion/InsertSingleCollection'
 
 export default {
 	name: 'Collections',
 	components: {
         CollectionsList,
-        SearchBar
-	}
+        SearchBar,
+        InsertSingleCollection
+    },
+    data() {
+        return {
+            displayInsertionFormButton: false
+        }
+    },
+    methods: {
+        displayInsertionForm() {
+            this.displayInsertionFormButton = !this.displayInsertionFormButton
+        }
+    }
 }
 </script>
